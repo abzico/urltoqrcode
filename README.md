a170d0956c71aa0955eea712e6a6ced5afcbf138
Wasin Thonkaew <haxpor@gmail.com>
2018-02-14 17:14:35 +0800

    Initial working version

---
+++ README.md
@@ -0,0 +1,9 @@
+[![donate button](https://img.shields.io/badge/$-donate-ff69b4.svg?maxAge=2592000&amp;style=flat)](https://github.com/haxpor/donate)
+
+# urltocode
+
+A simple URL to QRCode converter for the current browser tab's URL.
+
+# License
+
+MIT license. abzi.co
---
+++ main.js
@@ -0,0 +1,11 @@
+// setting to generate qrcode
+var qrcode = new QRCode(document.getElementById("qrcode"), {
+	width : 256,
+	height : 256
+});
+
+// get url from current tab
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+    qrcode.makeCode(url);
+});
